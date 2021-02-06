03dddfb8eb97f1575601c809ea5f3412c40bff05
baipc <chandra.baipalli@in.pega.com>
2021-02-06 15:00:55 +0530

    sample wensocket

---
+++ README.md
@@ -0,0 +1,15 @@
+# Setup
+
+
+This project captures basic introduction of websockets with springboot.
+
+# Server start
+
+1. Clone project
+2. Start server 
+```shell
+> ./gradlew bootRun
+```
+3. Access server as
+   http://localhost:8080/
+   
---
+++ src/main/java/com/chandra/websockets/Greeting.java
@@ -0,0 +1,17 @@
+package com.chandra.websockets;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
